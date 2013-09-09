573d41ec3f6c25c68d4da8d9b1b1e5ac6cbd84e5
8one6 <jbrodie@8one6.com>
2013-09-08 20:47:43 -0400

    Did a collectstatic to make sure everything was working

---
+++ www_static/js/patch.js
@@ -0,0 +1,14 @@
+function dismissRelatedLookupPopup(win, chosenId) {
+    var name = windowname_to_id(win.name);
+    var elem = document.getElementById(name);
+    if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
+        elem.value += ',' + chosenId;
+    } else {
+        document.getElementById(name).value = chosenId;
+    }
+	document.getElementById(name).trigger("liszt:updated");
+    win.close();
+	alert('I got here!')
+}
+
+alert('We got here!')
