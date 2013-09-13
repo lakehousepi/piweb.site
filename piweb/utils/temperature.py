0fdbf7f2abb7eb90753c6dd1bad6262de9216346
8one6 <jbrodie@8one6.com>
2013-09-12 20:20:28 -0400

    Creating config files and updating temperature.py to have TempReader class

--- piweb/utils/temperature.py
+++ piweb/utils/temperature.py
@@ -2,30 +2,36 @@ import os
 import glob
 import time
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+class TempReader(object):
+	def __init__(self, temp_sensor_pin):
+		os.system('modprobe w1-gpio')
+		os.system('modprobe w1-therm')
+		
+		self.temp_sensor_pin = temp_sensor_pin
+		self.base_dir  = '/sys/bus/w1/devices/'
+		self.device_folder = glob.glob(
+			base_dir + 
+			str(self.temp_sensor_pin) +
+			'*')[0]
+		self.device_file = device_folder + '/w1_slave'
 
-base_dir  = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+	def read_temp_raw(self):
+		f = open(self.device_file, 'r')
+		lines = f.readlines()
+		f.close()
+		return lines
 
-def read_temp_raw():
-	f = open(device_file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
-
-def read_temp(c_or_f='C'):
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = read_temp_raw()
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string)/1000.0
-	if c_or_f.upper() == 'F':
-		temp_f = (temp_c * 180.0/100.0) + 32.0
-		return temp_f
-	else:
-		return temp_c
+	def read_temp(self, c_or_f='C'):
+		lines = self.read_temp_raw()
+		while lines[0].strip()[-3:] != 'YES':
+			time.sleep(0.2)
+			lines = read_temp_raw()
+		equals_pos = lines[1].find('t=')
+		if equals_pos != -1:
+			temp_string = lines[1][equals_pos+2:]
+			temp_c = float(temp_string)/1000.0
+		if c_or_f.upper() == 'F':
+			temp_f = (temp_c * 180.0/100.0) + 32.0
+			return temp_f
+		else:
+			return temp_c
