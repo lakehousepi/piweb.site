719422e468193bdbf84e2938f47cc191bb1f2bb8
8one6 <jbrodie@8one6.com>
2013-09-13 22:30:48 -0400

    Continue work on hourly script

--- piweb/utils/ip.py
+++ piweb/utils/ip.py
@@ -11,3 +11,9 @@ def global_ip_from_jsonip():
 def local_ip():
 	ip = netifaces.ifaddresses("wlan0").get(netifaces.AF_INET)[0]['addr']
 	return ip
+	
+def both_ip():
+	return {
+		'local_ip': local_ip(),
+		'global_ip': global_ip_from_jsonip()
+	}
--- piweb/utils/scripts/hourly.py
+++ piweb/utils/scripts/hourly.py
@@ -1,20 +1,30 @@
 import json
+import time
 
 from utils.email import EmailSender
 from utils.gspreadsheet import GSpreadsheetUpdater
-from utils.ip import global_ip_from_jsonip, local_ip
+from utils.ip import both_ip
 from utils.led import LEDController
 from utils.temperature import TempReader
 
 from utils.config import gdocs, pins
 
 def hourly():
-	tr = TempReader(temp_sensor_pin=pins.TEMP_SENSOR)
-	tempdict = tr.read_temp()
-	tempstring = json.dumps(tempdict)
+	green = LEDController(led_pin=pins.GREEN_LED)
+	green.on()
 	
+	datadict = {}
+	
+	tr = TempReader(temp_sensor_pin=pins.TEMP_SENSOR)
+	datadict.update(tr.read_temp())
+	
+	datadict.update(both_ip())
+	
+	
+	datastring = json.dumps(datadict)
 	es = EmailSender(servername=gdocs.SERVERNAME, username=gdocs.USERNAME,
 			password=gdocs.PASSWORD)
 	es.sendmail(fromaddr=gdocs.USERNAME, toaddrs=['jbrodie@gmail.com'],
-			subject='Temperature update', body=tempstring)
-			
+			subject='Temperature update', body=datastring)
+	
+	green.off()
