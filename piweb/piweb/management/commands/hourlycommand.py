ddb9f7788e20313d726a61b995365ea6e362b499
8one6 <jbrodie@8one6.com>
2013-09-21 21:10:43 -0400

    Added **options to handle_noargs in hourlycommand

--- piweb/piweb/management/commands/hourlycommand.py
+++ piweb/piweb/management/commands/hourlycommand.py
@@ -1,5 +1,6 @@
 from django.core.management.base import NoArgsCommand
 from utils.scripts.hourly import hourly
+
 class Command(NoArgsCommand):
-	def handle_noargs(self):
+	def handle_noargs(self, **options):
 		hourly()
