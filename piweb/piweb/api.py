b77605fde35d857f2aa9087c564af3542b90df19
8one6 <jbrodie@8one6.com>
2013-09-22 14:55:43 -0400

    Try tastypie resources

--- piweb/piweb/api.py
+++ piweb/piweb/api.py
@@ -1,7 +1,22 @@
 from tastypie.resources import ModelResource
-from piweb.models import TempReading
+from piweb.models import IPReading, IPSeries, TempReading, TempSeries
+
+class IPReadingResource(ModelResource):
+	class Meta:
+		queryset = IPReading.objects.all()
+		resource_name = 'ipreading'
+
+class IPSeriesResource(ModelResource):
+	class Meta:
+		queryset = IPSeries.objects.all()
+		resource_name = 'ipseries'
 
 class TempReadingResource(ModelResource):
 	class Meta:
 		queryset = TempReading.objects.all()
 		resource_name = 'tempreading'
+		
+class TempSeriesResource(ModelResource):
+	class Meta:
+		queryset = TempSeries.objects.all()
+		resource_name = 'tempseries'
--- piweb/piweb/urls.py
+++ piweb/piweb/urls.py
@@ -1,5 +1,15 @@
 from django.conf.urls import patterns, include, url
 
+from tastypie.api import Api
+from piweb.api import IPReadingResource, IPSeriesResource, TempReadingResource,
+	TempSeriesResource
+
+piweb_api = Api(api_name='piweb')
+piweb_api.register(IPReadingResource())
+piweb_api.register(IPSeriesResource())
+piweb_api.register(TempReadingResource())
+piweb_api.register(TempSeriesResource())
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +24,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+	# Tastypie API hookup
+	url(r'^api/', include(piweb_api.urls)),
 )
