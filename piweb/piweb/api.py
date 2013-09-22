5bf25fd98fdebc8c393511a2cd5fba117fde23f1
8one6 <jbrodie@8one6.com>
2013-09-22 15:12:31 -0400

    Trying to put needed declarations earlier

--- piweb/piweb/api.py
+++ piweb/piweb/api.py
@@ -2,24 +2,24 @@ from tastypie.resources import ModelResource
 from tastypie import fields
 from piweb.models import IPReading, IPSeries, TempReading, TempSeries
 
+class IPSeriesResource(ModelResource):
+	class Meta:
+		queryset = IPSeries.objects.all()
+		resource_name = 'ipseries'
+
 class IPReadingResource(ModelResource):
 	ipseries = fields.ForeignKey(IPSeriesResource, 'ipseries')
 	class Meta:
 		queryset = IPReading.objects.all()
 		resource_name = 'ipreading'
 
-class IPSeriesResource(ModelResource):
+class TempSeriesResource(ModelResource):
 	class Meta:
-		queryset = IPSeries.objects.all()
-		resource_name = 'ipseries'
+		queryset = TempSeries.objects.all()
+		resource_name = 'tempseries'
 
 class TempReadingResource(ModelResource):
 	tempseries = fields.ForeignKey(TempSeriesResource, 'tempseries')
 	class Meta:
 		queryset = TempReading.objects.all()
 		resource_name = 'tempreading'
-		
-class TempSeriesResource(ModelResource):
-	class Meta:
-		queryset = TempSeries.objects.all()
-		resource_name = 'tempseries'
