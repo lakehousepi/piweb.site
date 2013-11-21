4095c14d2fd35548d11f541553a898100ebad610
8one6 <jbrodie@8one6.com>
2013-11-20 19:07:14 -0500

    Modifying test view

--- piweb/piweb/views.py
+++ piweb/piweb/views.py
@@ -2,3 +2,7 @@ from django.views.generic import TemplateView
 
 class TestView(TemplateView):
     template_name = 'piweb/test.html'
+    def get_context_data(self, **kwargs):
+        context = super(TestView, self).get_context_data(**kwargs)
+        context['svgtext'] = 'Hello New World'
+        return context
