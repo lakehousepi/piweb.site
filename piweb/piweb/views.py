0a7ee65210f27ff962140c3e0e6c5562c92c94a7
8one6 <jbrodie@8one6.com>
2013-11-21 01:15:35 -0500

    Trying to show an image

--- piweb/piweb/views.py
+++ piweb/piweb/views.py
@@ -1,11 +1,29 @@
 from django.views.generic import TemplateView
 from piweb.models import TempReading, TempSeries
+import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import StringIO
 
 class TestView(TemplateView):
     template_name = 'piweb/test.html'
     def get_context_data(self, **kwargs):
+        data = np.random.randn(10) * 20
+        x = data + np.random.randn(10)
+        y = 3 * data + np.random.randn(10)
+        
+        fig = Figure()
+        ax = fig.add_subplot(1,1,1)
+        ax.scatter(x, y, c='b')
+        fig.set(facecolor=None)
+        canvas = FigureCanvas(fig)
+        
+        imgdata = StringIO.StringIO()
+        canvas.print_svg(imgdata)
+        
+        imgstr = imgdata.get_data()
+        
         context = super(TestView, self).get_context_data(**kwargs)
-        context['svgtext'] = 'Hello New World'
+        context['svgtext'] = imgstr
+        
         return context
