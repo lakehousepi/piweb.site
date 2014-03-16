f5038fcc95145273663159e3f63febac5e2252d4
Josh Brodie <jbrodie@8one6.com>
2014-03-16 19:18:02 +0000

    Improving graphing with shorter history

--- piweb/piweb/views.py
+++ piweb/piweb/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 from piweb.models import TempReading, TempSeries
 import numpy as np
 import pandas as pd
+import matplotlib
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import seaborn as sbn
@@ -11,11 +12,13 @@ class TestView(TemplateView):
     template_name = 'piweb/test.html'
     def get_context_data(self, **kwargs):
         upstairs = TempSeries.objects.get(name='Upstairs')
-        upstairstemps = upstairs.tempreading_set.all().order_by('-timestamp')
+        upstairstemps = upstairs.tempreading_set.all().order_by('-timestamp')[:300]
 
         frame = pd.DataFrame(list(upstairstemps.values()))
         frame.set_index('timestamp', inplace=True)
        
+        matplotlib.rcParams['svg.fonttype'] = 'none'
+
         fig = Figure()
         ax = fig.add_subplot(1,1,1)
         frame['value'].plot(ax=ax)
