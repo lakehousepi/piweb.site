edca79dcceb861439458990d5ee622568eacb726
8one6 <jbrodie@8one6.com>
2014-11-01 15:26:01 -0400

    Test

--- piweb/piweb/views.py
+++ piweb/piweb/views.py
@@ -64,8 +64,9 @@ class FourChartsView(TemplateView):
         pngstr = pngdata.getvalue()
         
         context = super(FourChartsView, self).get_context_data(**kwargs)
-        context['svgtext'] = svgstr
+        context['svgtext'] = None #svgstr
         context['pngtext'] = pngstr
+        assert(1==0)
 
         return context
 
