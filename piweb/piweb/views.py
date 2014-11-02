e4ea330e4cabd1a5951135ca17164813f14c12ff
8one6 <jbrodie@8one6.com>
2014-11-02 10:03:19 -0500

    Rotating axis labels

--- piweb/piweb/views.py
+++ piweb/piweb/views.py
@@ -53,9 +53,7 @@ class FourChartsView(TemplateView):
 
             ax.grid(b=True, which='major', color='w', linewidth=1.5)
             ax.grid(b=True, which='minor', color='w', linewidth=0.75)
-        
-            ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=30)
-                      
+                              
         pngdata = StringIO.StringIO()
         fig.savefig(pngdata, format='png', facecolor='w', bbox_inches='tight')
         
