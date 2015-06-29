8c991886145da3ab49b1c0e0dc050cdf4c33d558
8one6 <jbrodie@8one6.com>
2015-06-29 02:57:14 -0400

    Adding new graphing file

--- piweb/piweb/views.py
+++ piweb/piweb/views.py
@@ -5,12 +5,6 @@ from piweb.models import TempReading, TempSeries
 import numpy as np
 import pandas as pd
 
-import matplotlib as mpl
-mpl.use('Agg')
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import matplotlib.pyplot as plt
-import seaborn as sbn
-
 import StringIO
 import base64
 import psutil
@@ -19,9 +13,11 @@ import pytz
 from dateutil.relativedelta import relativedelta
 from debug_toolbar_line_profiler import profile_additional
 
+from piweb.utils import graphing as pwgraph
+
 class HomeView(TemplateView):
     template_name = 'piweb/home.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         return context
@@ -30,41 +26,11 @@ class FourChartsView(TemplateView):
     template_name = 'piweb/fourcharts.html'
 
     def get_context_data(self, **kwargs):
-        now = dt.datetime.now(pytz.timezone('America/New_York'))
-        ayearago = now - relativedelta(years=1)
-        
-        upstairs = TempSeries.objects.get(name='Upstairs')
-        upstairstemps = upstairs\
-                            .tempreading_set.filter(timestamp__gte=ayearago)\
-                            .order_by('timestamp')
-        
-        df = pd.DataFrame(list(upstairstemps.values()))
-        df = df.set_index('timestamp')
-                
-        fig, axes = plt.subplots(2, 2, figsize=(15, 8), dpi=200)
-        for (i, d), rsamp in zip(enumerate([360, 30, 7, 1]), ['d', 'h', None, None]):
-            ax = axes.flatten()[i]
-            earlycut = now - relativedelta(days=d)
-            data = df.loc[df.index>=earlycut, :]
-            if rsamp:
-                data = data.resample(rule=rsamp, how='mean')
-            ax.plot(data.index, data['value'])
-            
-            ax.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
-            ax.get_yaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
 
-            ax.grid(b=True, which='major', color='w', linewidth=1.5)
-            ax.grid(b=True, which='minor', color='w', linewidth=0.75)
-                              
-            plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
-            
-            ax.set_title('Temperature data going back %d days' % d)
-
-        fig.tight_layout()
 
         pngdata = StringIO.StringIO()
         fig.savefig(pngdata, format='png', facecolor='w', bbox_inches='tight')
-        
+
         context = super(FourChartsView, self).get_context_data(**kwargs)
         # context['svgstr'] = svgstr
         context['pngstr'] = base64.b64encode(pngdata.getvalue())
@@ -74,7 +40,7 @@ class FourChartsView(TemplateView):
 
 class TestView(TemplateView):
     template_name = 'piweb/test.html'
-    
+
     def get_context_data(self, **kwargs):
         lim = self.request.GET.get('limit', 3000)
 
@@ -83,7 +49,7 @@ class TestView(TemplateView):
 
         frame = pd.DataFrame(list(upstairstemps.values()))
         frame.set_index('timestamp', inplace=True)
-        
+
         # matplotlib.rcParams['svg.fonttype'] = 'none'
 
         fig, ax = plt.subplots(1,1, figsize=(10, 10))
@@ -96,7 +62,7 @@ class TestView(TemplateView):
 
         svgdata = StringIO.StringIO()
         fig.savefig(svgdata, format='svg', facecolor='w', bbox_inches='tight')
-        
+
         context = super(TestView, self).get_context_data(**kwargs)
         # context['svgstr'] = svgstr
         context['svgtext'] = svgdata.getvalue()
