3ace6f14be65aa1153fafbd8832864defbee5e3a
8one6 <jbrodie@8one6.com>
2015-07-04 16:43:05 -0400

    Try adding html emailer ability

--- piweb/utils/emailer.py
+++ piweb/utils/emailer.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 class EmailSender(object):
     def __init__(self, servername, username, password):
@@ -10,11 +12,11 @@ class EmailSender(object):
         self.server = smtplib.SMTP(self.servername)
         self.server.starttls()
         self.server.login(self.username, self.password)
-    
+
     def logout(self):
         self.server.quit()
         self.server = None
-    
+
     def sendmail(self, fromaddr, toaddrs, subject, body):
         msg = 'From: ' + fromaddr + '\n'
         msg += 'To: ' + ', '.join(toaddrs) + '\n'
@@ -22,4 +24,34 @@ class EmailSender(object):
         msg += body
         self.login()
         self.server.sendmail(fromaddr, toaddrs, msg)
+        self.logout()
+
+    def sendmail_html(self, fromaddr, toaddrs, subject, textbody=None, htmlbody=None):
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = fromaddr
+        msg['To'] = ', '.join(toaddrs)
+
+        if textbody is None:
+            textbody = 'Hello World'
+        if htmlbody is None:
+            htmlbody = """\
+                        <html>
+                          <head></head>
+                          <body>
+                            <p>Hi!<br>
+                               How are you?<br>
+                               Here is the <a href="http://www.python.org">link</a> you wanted.
+                            </p>
+                          </body>
+                        </html>
+                        """
+
+        part1 = MIMEText(textbody, 'plain')
+        part2 = MIMEText(htmlbody, 'html')
+
+        msg.attach(part1)
+        msg.attach(part2)
+        self.login()
+        self.server.sendmail(fromaddr, toaddrs, msg.as_string())
         self.logout()
