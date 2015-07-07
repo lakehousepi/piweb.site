4dbdd30d78fffc3ed57133ad952a383ef2a6decb
8one6 <jbrodie@8one6.com>
2015-07-06 21:47:11 -0400

    change order

--- piweb/utils/scripts/infoemail.py
+++ piweb/utils/scripts/infoemail.py
@@ -126,6 +126,6 @@ def infoemail_html_with_image():
             password=gdocs.PASSWORD)
     es.sendmail_html_with_image(fromaddr=gdocs.USERNAME, toaddrs=infoemaillist,
             subject='Temperature update (html)', textbody=datastring,
-            htmlbody=h, imagepath=os.path.join(settings.STATIC_ROOT, images, 'fourgraphs.png'))
+            htmlbody=h, imagepath=os.path.join(settings.STATIC_ROOT, 'images', 'fourgraphs.png'))
 
     green.off()
