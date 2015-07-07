e63bdb4bcd10f8dfb818f708a3c5b565485e88bb
8one6 <jbrodie@8one6.com>
2015-07-06 20:17:49 -0400

    improved html email template

--- piweb/piweb/management/commands/infoemail.py
+++ piweb/piweb/management/commands/infoemail.py
@@ -1,6 +1,6 @@
 from django.core.management.base import NoArgsCommand
-from utils.scripts.infoemail import infoemail
+from utils.scripts.infoemail import infoemail_html
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        infoemail()
+        infoemail_html()
