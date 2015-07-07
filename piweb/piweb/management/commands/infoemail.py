f93c65ba3c721fbb70920596506adcebfe82a172
Joshua Brodie <jbrodie@UBUPRDAPP01.(none)>
2015-07-07 10:34:06 -0400

    Changing email to include attachment

--- piweb/piweb/management/commands/infoemail.py
+++ piweb/piweb/management/commands/infoemail.py
@@ -1,6 +1,6 @@
 from django.core.management.base import NoArgsCommand
-from utils.scripts.infoemail import infoemail_html
+from utils.scripts.infoemail import infoemail_html_with_image
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        infoemail_html()
+        infoemail_html_with_image()
